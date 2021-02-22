078993657bb8f0610b2fd01362ba4f54f2a112ab
palsmaker <pallaviboruah@gmail.com>
2021-02-22 17:21:37 -0600

    Updated

--- src/main/java/com/page/AddCategoryPage.java
+++ src/main/java/com/page/AddCategoryPage.java
@@ -1,19 +1,34 @@
 package com.page;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import com.base.TestBase;
 
 public class AddCategoryPage extends TestBase{
 	
-	@FindBy(xpath="//input[@name='categorydata']")
+	@FindBy(name="categorydata")
 	WebElement AddCategory;
 	
 	@FindBy(xpath="//input[@type='submit']")
 	WebElement AddCategoryButton;
+	
+	@FindBy(xpath="//input[@type='checkbox' and @name='allbox']")
+	WebElement ToggleAllButton;
+	
+	//@FindBy(xpath="//input[contains(text(),' Toggle all ')]")
+	//WebElement ToggleAllLabel;
+	
+	@FindBy(xpath="//input[@type= 'submit' and @value='Add category']")
+		WebElement AddCategoryLabel;
+	
+	@FindBy(name="due_month")
+	WebElement MonthDropDownLocator;
+	
 	
 	public AddCategoryPage() {
 		PageFactory.initElements(driver, this);
@@ -28,6 +43,23 @@ public class AddCategoryPage extends TestBase{
 		AddCategoryButton.click();
 		
 	}
+	
+	public void ToggleAllBox() {
+		ToggleAllButton.click();
+	}
+	
+	//public boolean VerifyToggleAllLabel() {
+		//return ToggleAllLabel.isDisplayed();}
+	
+	public boolean verifyAddCustomerLabel() {
+		return AddCategoryLabel.isDisplayed();	
+	}
+	
+	public boolean MonthDropDownLocatorOption() {
+		return MonthDropDownLocator.isEnabled();
+	}
+	
+
 	
 
 
--- src/test/java/com/test/AddCategoryPageTest.java
+++ src/test/java/com/test/AddCategoryPageTest.java
@@ -1,5 +1,8 @@
 package com.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,6 +12,7 @@ import com.base.TestBase;
 import com.page.AddCategoryPage;
 
 public class AddCategoryPageTest extends TestBase {
+	
 	
 	AddCategoryPage addCategoryPage;
 	
@@ -23,18 +27,54 @@ public class AddCategoryPageTest extends TestBase {
 		addCategoryPage = new AddCategoryPage();		
 	}
 	
-	@Test
+	@Test(priority=1)
 	public void EnterNewCategoryTest() {
 		String newCategory = addCategoryPage.EnterNewCategory();
 		Assert.assertEquals(newCategory, "Pink");
 		System.out.println("Pink");	
 	}
 	
-	@Test
+	@Test (priority=3)
 	public void EnterAddCategoryButton() {
 		addCategoryPage.EnterAddCategoryButon();
 	
 	}
+	
+	@Test(priority=2)
+	public void EnterToggleAllButton() {
+		addCategoryPage.ToggleAllBox();
+	}
+	
+	/*
+	 * @Test public void VerifyToggleAllTest() { Boolean labelDetails =
+	 * addCategoryPage.VerifyToggleAllLabel(); Assert.assertTrue(labelDetails); }
+	 */
+	
+	@Test
+	public void VerifyAddCustomerLabelTest() {
+		Boolean Label = addCategoryPage.verifyAddCustomerLabel();
+		Assert.assertTrue(Label);
+		
+	}
+	
+
+	/*
+	 * Select sel = new
+	 * Select(driver.findElement(By.xpath("//[@name='due_month']")));
+	 * sel.selectByValue("3");
+	 * 
+	 * 
+	 * if(sel.isMultiple()){
+	 * 
+	 * //Selecting multiple values by index sel.selectByValue("1");
+	 * sel.selectByValue("2"); sel.selectByValue("3"); sel.selectByValue("4");
+	 * sel.selectByValue("5"); sel.selectByValue("6"); sel.selectByValue("7");
+	 * sel.selectByValue("8"); sel.selectByValue("9"); sel.selectByValue("10");
+	 * sel.selectByValue("11"); sel.selectByValue("12"); }
+	 * 
+	 * 
+	 */
+
 
 	
 	@AfterMethod
